73e757805a381eb440ef8a0e9d2b5ddb8a58ab87
LanguageByAamir <zardariaamirali200@gmail.com>
2024-10-27 22:09:32 +0500

    Add files via upload

---
+++ greaternumber.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    // Input three numbers from the user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("Enter third number: ");
+    scanf("%d", &num3);
+
+    // Determine the largest number using nested if-else
+    if (num1 >= num2) {
+        if (num1 >= num3) {
+            printf("%d is the largest number.\n", num1);
+        } else {
+            printf("%d is the largest number.\n", num3);
+        }
+    } else {
+        if (num2 >= num3) {
+            printf("%d is the largest number.\n", num2);
+        } else {
+            printf("%d is the largest number.\n", num3);
+        }
+    }
+
+    return 0;
+}
